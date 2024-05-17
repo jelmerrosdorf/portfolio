2d520b0ba84151d7a3ca0c338324a34ea6b957bc
Jelmer Rosdorf <jelmerr99@gmail.com>
2024-05-17 14:54:49 +0200

    Add full portfolio website

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let navToggle = document.querySelector('.nav-toggle');
+let navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+
